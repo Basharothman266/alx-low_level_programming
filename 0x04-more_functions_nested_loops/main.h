70470aa781bd1de5e508b4624696e46060e4d792
bashar othamn <bashar0776@gmil.com>
2023-07-20 10:03:54 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN H
+
+int_putchar(char c);
+
+int supper(int c); int_isdigit(int c);
+
+int mul(int a, int b);
+
+void print numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n). void print diagonal(int n);
+
+void print square(int size);
+
+void print triangle (int size);
+
+#endif
